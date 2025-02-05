14018ee935f052b8e1d801d63c9a997cb74b3cd5
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2025-02-05 17:14:28 +0300

    Iyzico

--- Sarideniz.WebUI/Controllers/CartController.cs
+++ Sarideniz.WebUI/Controllers/CartController.cs
@@ -1,3 +1,6 @@
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sarideniz.Core.Entities;
@@ -5,17 +8,18 @@ using Sarideniz.Service.Abstract;
 using Sarideniz.Service.Concrete;
 using Sarideniz.WebUI.ExtensionMethods;
 using Sarideniz.WebUI.Models;
+using Address = Sarideniz.Core.Entities.Address;
 
 namespace Sarideniz.WebUI.Controllers;
 
 public class CartController : Controller
 {
     private readonly IService<Product> _serviceProduct;
-    private readonly IService<Address> _serviceAddress;
+    private readonly IService<Core.Entities.Address> _serviceAddress;
     private readonly IService<AppUser> _serviceAppUser;
     private readonly IService<Order> _serviceOrder;
 
-    public CartController(IService<Product> serviceProduct, IService<Address> serviceAddress,
+    public CartController(IService<Product> serviceProduct, IService<Core.Entities.Address> serviceAddress,
         IService<AppUser> serviceAppUser, IService<Order> serviceOrder)
     {
         _serviceProduct = serviceProduct;
@@ -155,6 +159,98 @@ public class CartController : Controller
                 UnitPrice = item.Product.Price,
             });
         }
+
+        #region OdemeIslemi
+
+        Options options = new Options();
+        options.ApiKey = "your api key";
+        options.SecretKey = "your secret key";
+        options.BaseUrl = "https://sandbox-api.iyzipay.com";
+
+        CreatePaymentRequest request = new CreatePaymentRequest();
+        request.Locale = Locale.TR.ToString();
+        request.ConversationId = "123456789";
+        request.Price = "1";
+        request.PaidPrice = "1.2";
+        request.Currency = Currency.TRY.ToString();
+        request.Installment = 1;
+        request.BasketId = "B67832";
+        request.PaymentChannel = PaymentChannel.WEB.ToString();
+        request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+
+        PaymentCard paymentCard = new PaymentCard();
+        paymentCard.CardHolderName = "John Doe";
+        paymentCard.CardNumber = "5528790000000008";
+        paymentCard.ExpireMonth = "12";
+        paymentCard.ExpireYear = "2030";
+        paymentCard.Cvc = "123";
+        paymentCard.RegisterCard = 0;
+        request.PaymentCard = paymentCard;
+
+        Buyer buyer = new Buyer();
+        buyer.Id = "BY789";
+        buyer.Name = "John";
+        buyer.Surname = "Doe";
+        buyer.GsmNumber = "+905350000000";
+        buyer.Email = "email@email.com";
+        buyer.IdentityNumber = "74300864791";
+        buyer.LastLoginDate = "2015-10-05 12:43:35";
+        buyer.RegistrationDate = "2013-04-21 15:12:09";
+        buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        buyer.Ip = "85.34.78.112";
+        buyer.City = "Istanbul";
+        buyer.Country = "Turkey";
+        buyer.ZipCode = "34732";
+        request.Buyer = buyer;
+
+        var shippingAddress = new Iyzipay.Model.Address();
+        shippingAddress.ContactName = "Jane Doe";
+        shippingAddress.City = "Istanbul";
+        shippingAddress.Country = "Turkey";
+        shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        shippingAddress.ZipCode = "34742";
+        request.ShippingAddress = shippingAddress;
+
+        var billingAddress = new Iyzipay.Model.Address();
+        billingAddress.ContactName = "Jane Doe";
+        billingAddress.City = "Istanbul";
+        billingAddress.Country = "Turkey";
+        billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+        billingAddress.ZipCode = "34742";
+        request.BillingAddress = billingAddress;
+
+        List<BasketItem> basketItems = new List<BasketItem>();
+        BasketItem firstBasketItem = new BasketItem();
+        firstBasketItem.Id = "BI101";
+        firstBasketItem.Name = "Binocular";
+        firstBasketItem.Category1 = "Collectibles";
+        firstBasketItem.Category2 = "Accessories";
+        firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+        firstBasketItem.Price = "0.3";
+        basketItems.Add(firstBasketItem);
+
+        BasketItem secondBasketItem = new BasketItem();
+        secondBasketItem.Id = "BI102";
+        secondBasketItem.Name = "Game code";
+        secondBasketItem.Category1 = "Game";
+        secondBasketItem.Category2 = "Online Game Items";
+        secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
+        secondBasketItem.Price = "0.5";
+        basketItems.Add(secondBasketItem);
+
+        BasketItem thirdBasketItem = new BasketItem();
+        thirdBasketItem.Id = "BI103";
+        thirdBasketItem.Name = "Usb";
+        thirdBasketItem.Category1 = "Electronics";
+        thirdBasketItem.Category2 = "Usb / Cable";
+        thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+        thirdBasketItem.Price = "0.2";
+        basketItems.Add(thirdBasketItem);
+        request.BasketItems = basketItems;
+
+        Payment payment = await Payment.Create(request, options);
+
+        #endregion
 
         // Sipariş kaydetme
         try
